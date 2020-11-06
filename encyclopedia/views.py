7c01774ae7f07d052d8e0a96b55fad36c99c9c70
Michael <michael_schneemann@hotmail.de>
2020-11-06 23:48:04 +0100

    added a search functionality. 1st step: exact matching done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
 
+class NewSearchForm(forms.Form):
+    search = forms.CharField(label="New Search")
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    if request.method == "POST":
+        form = NewSearchForm(request.POST)
+        if form.is_valid():
+            searchInput = form.cleaned_data['search']
+            content = util.get_entry(searchInput)
+        
+            return render(request, "encyclopedia/entry.html", {
+                "entryTitle": searchInput, "entryContent": content
+            })
+        else:
+            return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(),
+            "form": NewSearchForm()
+        })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(),
+            "form": NewSearchForm()
+        })
+
 
 def entry(request, title):
     content = util.get_entry(title)
-
     if content is not None:
         return render(request, "encyclopedia/entry.html", {
             "entryTitle": title, "entryContent": content
         })
     else:
-        
+
         return render(request, "encyclopedia/error.html", {
             "title": title
         })
